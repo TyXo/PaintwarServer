a42ff37e2012c07b0ff353ba142e1ec8ff9424cd
TyXo <tixo_xi@hotmail.com>
2019-04-25 02:34:46 -0300

    Server connection number

--- game.js
+++ game.js
@@ -1,25 +1,30 @@
 const availableColors = ["red", "green", "blue", "purple", "brown", "yellow"];
-
-function onClientConnect(id, reply, broadcast) {
-  if (availableColors.length === 0) {
-    return;
-  }
-  reply({
-    type: "connected",
-    id,
-    color: availableColors.pop()
-  });
-}
+nConnections = 0;
+function onClientConnect(id, reply, broadcast) {}
 
 function onClientDisconnect(id, broadcast) {
   broadcast({ type: "disconnected", id });
+  nConnections -= 1;
 }
 
-function onClientMessage(id, data, reply, broadcast) {
-  broadcast({
-    type: "synchronize",
-    data
-  });
+function onClientMessage(id, message, reply, broadcast) {
+  if (message.type === "connect") {
+    if (availableColors.length === 0) {
+      return;
+    }
+    nConnections++;
+    reply({
+      type: "connected",
+      id,
+      color: availableColors.pop(),
+      nConnections
+    });
+  } else if (message.type === "broadcast") {
+    broadcast({
+      type: "synchronize",
+      game: message.game
+    });
+  }
 }
 
 module.exports = {
